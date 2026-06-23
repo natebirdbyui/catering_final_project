d83334b68fa3291b0f39795a559ffbc5443acccc
natebirdbyui <nathan.bird@hotmail.com>
2026-06-22 20:47:59 -0600

    test update for main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ import { loadFooter } from "./footer.js";
 import { loadSlideshow, startSlideshow } from "./slideshow.js";
 
 console.log("main.js loaded");
-
+console.log("test to see if it updates in real time");
 loadHeader();
 loadFooter();
 loadSlideshow();
