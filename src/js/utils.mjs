2edd7628b92214d682c00767bd3b783d97cebf4b
natebirdbyui <nathan.bird@hotmail.com>
2026-06-25 18:12:25 -0600

    nav name fixes

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -8,13 +8,13 @@ export function loadNav() {
         <nav class="main-nav">
             <a href="${BASE}">Home</a>
             <a href="${BASE}menu/menu.html">Menu</a>
-            <a href="${BASE}about_us/about_us.html">About</a>
+            <a href="${BASE}about_us/about_us.html">About Us</a>
             <a href="${BASE}booking/booking.html">Booking</a>
-            <a href="${BASE}contact_us/contact_us.html">Contact</a>
+            <a href="${BASE}contact_us/contact_us.html">Contact Us</a>
         </nav>
 
         <nav class="cart-nav">
-            <a href="${BASE}cart/cart.html">🛒</a>
+            <a href="${BASE}cart/cart.html">🛒 Cart</a>
         </nav>
     `;
 }
